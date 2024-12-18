458ea0cc76e0606bc1caae79de895334f67833d4
Apricold <jhosgri@hotmail.com>
2024-12-18 18:02:08 -0500

    cambio

--- reports_microservice/routes/api.php
+++ reports_microservice/routes/api.php
@@ -16,6 +16,6 @@ use App\Http\Controllers\ReportsController;
 
 
 Route::get('/products-category', [ReportsController::class, 'ProductsWithCategory']);
-Route::get('/products-order-price', [ReportsController::class, 'ProductsOrdered']);
-Route::get('/products-zero-stock', [ReportsController::class, 'ProductsZeroStock']);
-Route::get('/products-stock-above-ten', [ReportsController::class, 'ProductStockTen']);
+Route::get('/products-price', [ReportsController::class, 'ProductsOrdered']);
+Route::get('/products-stock', [ReportsController::class, 'ProductsZeroStock']);
+Route::get('/products-stock-ten', [ReportsController::class, 'ProductStockTen']);
